587e33164a4a9ca1ef194e02ef553638d773f132
DFIRLore <92057957+DFIRLore@users.noreply.github.com>
2021-10-07 18:45:52 -0500

    Update Winevtfilter.py
    Changed the event ID to reflect connection to charger

--- Winevtfilter.py
+++ Winevtfilter.py
@@ -2,7 +2,7 @@ report = []
 with open("systemevt.txt", "r") as file: 
     for line in file: 
         splits = line.split('\t') 
-        if '107' in line: 
+        if '105' in line: 
             print(f'System power status changed {splits[1].rstrip()}') 
             report.append(f'Power status changed {splits[1]}') 
             
